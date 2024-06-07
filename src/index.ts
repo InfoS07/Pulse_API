b9cdf385d8a8ecc4bd2df1b54c43549cac6db2b0
L-Nicolas <nicolas.lacoste.esgi@gmail.com>
2024-06-07 18:16:04 +0200

    Ajout Authentification

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,17 @@ import schema from './schema';
 import defaultQuery from './queries/defaultQuery';
 import populateDatabase from './database/populate';
 import database from './database';
+import jwt from 'jsonwebtoken';
+import cors from 'cors';
 
 const app = express();
 
 const HOST: string = '0.0.0.0';
 const PORT: number = Number(process.env.PORT || 8080);
+const JWT_SECRET = process.env.JWT_SECRET;
+
+// Use CORS to allow requests from your Flutter app
+app.use(cors());
 
 /**
  * Middlewares
@@ -18,12 +24,40 @@ const PORT: number = Number(process.env.PORT || 8080);
 app.use(express.json());
 app.use(morgan('dev'));
 
-/* app.use('/', (_, res) => {
-  res.redirect('/graphql');
-}); */
+/**
+ * Middleware to verify JWT and restrict access to authenticated users
+ */
+const verifyToken = async (req: any, res: any, next: any) => {
+  const token = req.headers['authorization']?.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ error: 'Token missing' });
+  }
+
+  if (!JWT_SECRET) {
+    return res.status(500).json({ error: 'JWT secret missing' });
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, JWT_SECRET);
+    req.user = decodedToken;
+
+    const { data: user, error } = await database.auth.getUser(token);
+
+    if (error || !user) {
+      return res.status(401).json({ error: 'Invalid token' });
+    }
+
+    req.user = user;
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Invalid token' });
+  }
+};
 
 app.use(
   '/graphql',
+  verifyToken,
   graphqlHTTP({
     schema: schema,
     context: { database, viewer: null },
